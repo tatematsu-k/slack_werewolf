959a2e7c5d324b3f01979eb1b957c5fd42b37ee6
tatematsu-k <ynu1264@gmail.com>
2021-05-03 20:18:35 +0900

    refix structure for event factory

--- src/werewolf_event.rb
+++ src/werewolf_event.rb
@@ -1,7 +1,12 @@
 require './src/werewolf_event/start_game'
 
 class WerewolfEvent
+  CANDIDATE_CLASSES = [StartGame]
+
   def self.create(**args)
-    StartGame.new(**args)
+    CANDIDATE_CLASSES
+      .filter { |klass| klass.match?(**args) }
+      .first
+      .new(**args)
   end
 end
--- src/werewolf_event/start_game.rb
+++ src/werewolf_event/start_game.rb
@@ -2,6 +2,10 @@ require './src/werewolf_event/base'
 
 class WerewolfEvent
   class StartGame < Base
+    def self.match?(**args)
+      true
+    end
+
     def reply
       notifier.post(
         template: "start_game",
