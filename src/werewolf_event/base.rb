3202d3b84db40a939abecf1307a549ac1407daf2
tatematsu-k <ynu1264@gmail.com>
2021-05-03 20:36:19 +0900

    expand event for reply

--- src/werewolf_event/base.rb
+++ src/werewolf_event/base.rb
@@ -9,5 +9,12 @@ class WerewolfEvent
       self.text = text
       self.notifier = notifier
     end
+
+    def reply(**args)
+      notifier.post(
+        template: self.class.name.gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').downcase,
+        **args
+      )
+    end
   end
 end
